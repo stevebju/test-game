1cfb4d6110224b6ee6975aaad24b3aca6236d69a
stevebju <127677353+stevebju@users.noreply.github.com>
2023-03-12 10:17:43 -0500

    Initial files for MakeCode project

---
+++ NinjaSprite.ts
@@ -0,0 +1,19 @@
+// Add your code here
+const ninja = img`
+    1 1 1 1 1 1 f f f f f 1 1 1 1 1
+    1 1 1 1 1 f f f f f f f 1 1 1 1
+    1 1 f 1 f f f f f f f f f 1 1 1
+    1 1 1 f f b f f f f f b f 1 1 1
+    1 1 f 1 f d 1 1 b 1 1 d f 1 1 1
+    1 1 1 1 f f 1 f d f 1 f f 1 1 1
+    1 1 1 1 1 f f f f f f f 1 1 1 1
+    1 1 1 1 1 1 f f f f f 1 1 1 1 1
+    1 1 1 1 1 f f f f f f f 1 1 1 1
+    1 1 1 1 f f f f f f f f f 1 1 1
+    1 1 1 1 f f f f f f f f f 1 1 1
+    1 1 1 f 1 f f f f f f f 1 f 1 1
+    1 1 1 f 1 f f f f f f f 1 f 1 1
+    1 1 1 1 1 1 f f f f f 1 1 1 1 1
+    1 1 1 1 1 1 f f 1 f f 1 1 1 1 1
+    1 1 1 1 1 f f f 1 f f f 1 1 1 1
+    `;
---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "[z":
+            case "Ninja":return img`
+. . . . . . f f f f f . . . . . 
+. . . . . f f f f f f f . . . . 
+. . f . f f f f f f f f f . . . 
+. . . f f b f f f f f b f . . . 
+. . f . f d 1 1 b 1 1 d f . . . 
+. . . . f f 1 f d f 1 f f . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . . f f f f f . . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . f f f f f f f f f . . . 
+. . . . f f f f f f f f f . . . 
+. . . f . f f f f f f f . f . . 
+. . . f . f f f f f f f . f . . 
+. . . . . . f f f f f . . . . . 
+. . . . . . f f . f f . . . . . 
+. . . . . f f f . f f f . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
